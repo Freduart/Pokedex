9e829dc668dd5fc31300b0515b5cfb7e17c74faf
Freduart <afredxxstartrekxx@gmail.com>
2021-09-16 11:25:43 -0600

    Pokemon list
    Adding the pokemon list and the style for the card of every pokemon, adding the shared component and the onclick event for show the detail

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,27 +1,26 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
-import { PokemonList } from '../Models/pokemon.list';
+import { PokemonList } from "../models/pokemon.list";
+import { PokemonDetail } from "../models/pokemon.detail";
 import { map } from "rxjs/operators";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class PokemonService {
+    private baseUrl = 'https://pokeapi.co/api/v2/';
+    
+    constructor(private http: HttpClient) { }
 
-  //Conexion con la API de Pokemon
-  protected API_URL = `{ enviroment.apiURL/ }`;
 
-  constructor(private http: HttpClient) { }
+    getPokemonList(offset: number, limit: number = 20) : Observable<PokemonList[]> {
+        return this.http.get<PokemonList[]>(this.baseUrl + 'pokemon?offset=' + offset + '&limit=' + limit)
+        .pipe(
+            map((x: any) => x.results)
+        );
+    }
 
-  
-
-  //Trayendo la lista de pokemons con paginacion 
-  //Nos suscribimos a un observable para poder convertir la lista de pokemons 
-  getPokemonList(offset: number, limit: number=20): Observable<PokemonList[]>{
-    return this.http.get<PokemonList[]>(this.API_URL + 'pokemon?offset='+offset+'&limit='+limit)
-    .pipe(
-      map((x: any) => x.results)
-    );    
-  }  
+    getPokemonDetail(pokemon: number | string): Observable<PokemonDetail> {
+        return this.http.get<PokemonDetail>(this.baseUrl + 'pokemon/' + pokemon);
+    }
+    
 }
