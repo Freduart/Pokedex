159d29cd26c0ea048152b697d579198d1cb1bb08
Freduart <afredxxstartrekxx@gmail.com>
2021-09-16 14:04:12 -0600

    Pokemon detail
    Adding the Detail component for more specific pokemons  info

--- src/app/components/pokemon-detail/pokemon-detail.component.ts
+++ src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
+import { PokemonDetail } from '../../models/pokemon.detail';
 
 @Component({
   selector: 'app-pokemon-detail',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonDetailComponent implements OnInit {
 
-  constructor() { }
+  pokemon: PokemonDetail;
+  classicMode: boolean;
+
+  constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public data: any) { 
+    this.pokemon = data.pokemon;
+    this.classicMode = data.classicMode;
+  }
 
   ngOnInit(): void {
+  }
+
+
+  getAbilities(): string {
+    return this.pokemon.abilities.map(x => x.ability.name).join(', ');
+  }
+
+  getPrincipalType(list: any[]) {
+    return list.filter(x => x.slot === 1)[0]?.type.name;
   }
 
 }
